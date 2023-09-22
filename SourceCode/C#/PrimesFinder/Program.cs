a99a281c9e5b566405c7d237f65b31b1b944caa1
RL757 <harel.rubin2905@gmail.com>
2023-09-22 22:38:12 +0300

    change main

--- SourceCode/C#/PrimesFinder/Program.cs
+++ SourceCode/C#/PrimesFinder/Program.cs
@@ -136,7 +136,7 @@ class Program
     static void Main(string[] args)
     {
         // First Implementation 'should' be more memory efficient
-        PrimesUpTo(100000,true);
-        OriginalCode.PrimesUpTo(100000, true);
+        PrimesUpTo(1000000,true);
+        OriginalCode.PrimesUpTo(1000000, true);
     }
 }
